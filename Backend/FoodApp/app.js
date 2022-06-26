305617b6146e02edab151e5ae6a33cf066fe6107
Shrohit-1 <shrohitsoam@gmail.com>
2022-06-26 13:04:10 +0530

    Plans Implemented

--- Backend/FoodApp/app.js
+++ Backend/FoodApp/app.js
@@ -1,18 +1,16 @@
 const express= require('express');
-const path= require('path')
 const app= express();
 const cookieParser= require('cookie-parser')
+
 //for reading object which is passed in post
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 app.use(cookieParser())
 
 const userRouter= require('./Routers/userRouter')
-const authRouter= require('./Routers/authRouter');
+const planRouter=require('./Routers/planRouter');
 
-
-
-app.use("/auth",authRouter);
 app.use("/user",userRouter);
+app.use("/plans",planRouter);
 
 app.listen(8080);
---
+++ Backend/FoodApp/models/planModel.js
@@ -0,0 +1,43 @@
+const mongoose=require('mongoose');
+const {db_link}= require("../Secret/secret");
+
+mongoose.connect(db_link)
+.then(function(db){
+    console.log("DB CONNECTED");
+})
+.catch(function(err){
+    console.log(err);
+})
+
+const planSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+        maxLength:[20,'plan name should not exceed more than 20 charcter']
+    },
+    duration:{
+        type:Number,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:[true,'price not entered']
+    },
+    ratingAverage:{
+        type:Number
+    },
+    discount:{
+        type:Number,
+        validate:[function(){
+            return this.discount<100;
+        },'discount should not exceed price']
+    }
+});
+
+//model
+const planModel =mongoose.model('planModel',planSchema);
+module.exports= planModel;
+
+
+
