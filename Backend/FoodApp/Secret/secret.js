04610d350888c4de682d68f80ea5582b0a95d1d1
Shrohit-1 <shrohitsoam@gmail.com>
2022-07-02 08:20:54 +0530

    vf

--- Backend/FoodApp/Secret/secret.js
+++
@@ -1,3 +0,0 @@
-module.exports= { JWT_KEY:'fdsbfqy7hru2h327' ,
-                  db_link:'mongodb+srv://admin:2cUsHb2eT4X7699I@cluster0.mdwcxbg.mongodb.net/?retryWrites=true&w=majority'
-                }
